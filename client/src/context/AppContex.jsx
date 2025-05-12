67cb4499169c38ea7fe9b05015d82a499585a115
aman bhatt <amanb3588@gmail.com>
2025-05-12 10:17:19 +0530

    push

--- client/src/context/AppContex.jsx
+++ client/src/context/AppContex.jsx
@@ -2,6 +2,12 @@ import { createContext, useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyProducts } from "../assets/assets";
 import toast from "react-hot-toast";
+import axios from 'axios'
+
+
+
+axios.defaults.withCredentials=true;
+axios.defaults.baseURL=import.meta.env.VITE_BACKEND_URL;
 
 export const AppContext = createContext();
 
@@ -49,6 +55,20 @@ export const AppContextProvider = ({ children }) => {
     toast.success('Item removed from cart');
   };
 
+//fetch seller status
+const fetchSeller=async()=>{
+  try {
+    const {data}=await axios.get('/api/seller/is-auth');
+    if(data.success){
+      setIsSeller(true)
+    }else{
+      setIsSeller(false)
+    }
+  } catch (error) {
+    
+  }
+}
+
   const fetchProducts = async () => {
     setProducts(dummyProducts);
   };
@@ -77,6 +97,7 @@ export const AppContextProvider = ({ children }) => {
 
   useEffect(() => {
     fetchProducts();
+    fetchSeller()
   }, []);
 
 
@@ -96,7 +117,8 @@ export const AppContextProvider = ({ children }) => {
     searchQuery,
     setSearchQuery,
     getCartTotalAmount,
-    getCartItemsCount
+    getCartItemsCount,
+    axios
 
   };
 
